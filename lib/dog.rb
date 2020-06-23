5df6e88567b4213370f0b3d05518dd13fdb92773
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-23 02:09:45 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
+end
+
+
+
+class Dog
+  def initialize(name,breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+end
